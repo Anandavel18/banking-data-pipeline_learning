56ad8c76e100530c76cec237409bba4c39fe799d
Anandavel18 <san.anandavel@gmail.com>
2026-05-22 12:17:40 +0100

    trigger CI

--- Techincal_Exercise/script/arch_table_creation_script.sh
+++ Techincal_Exercise/script/arch_table_creation_script.sh
@@ -6,9 +6,14 @@ PROJECT_ID="project-b23f2a51-1258-4419-9bd"
 
 echo "Creating customers_arch table..."
 
+# Get the directory of this script
+SCRIPT_DIR=$(dirname "$0")
+SQL_DIR="$SCRIPT_DIR/../sql"
+
+# Use the relative path for the SQL file
 bq query \
---project_id=${PROJECT_ID} \
---use_legacy_sql=false \
-< /mnt/c/Users/sanan/OneDrive/Interview/banking_migration_project/Techincal_Exercise/sql/arch_table_create.sql
+  --project_id="${PROJECT_ID}" \
+  --use_legacy_sql=false \
+  < "$SQL_DIR/arch_table_create.sql"
 
 echo "All tables created successfully!"
